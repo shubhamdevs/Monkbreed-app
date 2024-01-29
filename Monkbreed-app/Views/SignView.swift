1ce046a3cefdb9d5857c8026cd731e0adc7eeba0
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-29 11:44:23 +0530

    rearrange input fields and login view layout

--- Monkbreed-app/Views/SignView.swift
+++ Monkbreed-app/Views/SignView.swift
@@ -13,34 +13,42 @@ struct SignView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20, content: {
+        VStack(alignment: .leading, spacing: 40, content: {
             Image(systemName: "arrow.left")
                 .imageScale(.large)
             
-            Text("Sign-in")
-                .font(.title)
+            VStack(alignment: .leading,spacing:15) {
+                Text("Sign-in")
+                    .font(.title)
+                
+                Text("Enter your email address and password and continue shopping.")
+                    .font(.callout)
+            }
             
-            Text("Enter your email address and password and continue shopping.")
-                .font(.callout)
+            VStack(spacing: 15) {
+                TextField("Enter Email Address", text: $email)
+                    .padding(.horizontal)
+                    .frame(height: 60)
+                    .background(.gray.opacity(0.1))
+                    .clipShape(Capsule())
+                    .overlay {
+                        Capsule()
+                            .stroke(.gray.opacity(0.8), lineWidth: 0.8)
+                    }
+                
+                SecureField("Enter Password", text: $password)
+                    .padding(.horizontal)
+                    .frame(height: 60)
+                    .background(.gray.opacity(0.1))
+                    .clipShape(Capsule())
+                    .overlay {
+                        Capsule()
+                            .stroke(.gray.opacity(0.8))
+                    }
+            }
             
-            TextField("Enter Email Address", text: $email)
-                .padding(.horizontal)
-                .frame(height: 60)
-                .background(.gray.opacity(0.1))
-                .clipShape(Capsule())
-                .overlay {
-                    Capsule()
-                        .stroke(.gray.opacity(0.8), lineWidth: 0.5)
-                }
-            SecureField("Enter Password", text: $password)
-                .padding(.horizontal)
-                .frame(height: 60)
-                .background(.gray.opacity(0.1))
-                .clipShape(Capsule())
-                .overlay {
-                    Capsule()
-                        .stroke(.gray.opacity(0.8))
-                }
+            Spacer()
+            
         })
         .padding()
         .preferredColorScheme(.dark)
