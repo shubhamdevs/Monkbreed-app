7dc8f67bce2a2e25e6ff7c17116433e4aefd7a15
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-29 09:39:54 +0530

    Initial Commit

---
+++ Monkbreed-app/Monkbreed_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monkbreed_appApp.swift
+//  Monkbreed-app
+//
+//  Created by Shubham Biswas on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Monkbreed_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
